4df40623be99f1f88522612e4439eb80fe335319
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-08-19 16:56:23 -0500

    dashboard snapshot complete

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,18 @@
 // Test away!
+import React from 'react';
+// Test away
+//import react-test-renderer as --dev dependency and then import...
+import renderer from 'react-test-renderer'; 
+import render from 'react-testing-library'; 
+import Controls from './Controls'; 
+
+describe('<Controls />', () => {
+    it('should match snapshot', () => {
+        const tree = renderer.create(<Controls />).toJSON(); 
+
+        expect(tree).toMatchSnapshot(); 
+    })
+    it('displays defaults as unlocked and closed', () => {
+        const tree = render(<Controls />); 
+    })
+})
