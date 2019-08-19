5e76a73ea034ced38ee0b456d8070248bac658fa
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-08-19 17:31:42 -0500

    finished snapshot for Display.spec.js

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,16 @@
-// Test away!
+
+import React from 'react';
+// Test away
+//import react-test-renderer as --dev dependency and then import...
+import renderer from 'react-test-renderer'; 
+import { render } from '@testing-library/react'; //import this for access to "render", instead of "render.create" for snapshots
+import Display from './Display'; //importing component we're testing
+
+describe('<Display />', () => {
+    it('should match snapshot', () => {
+        const tree = renderer.create(<Display />).toJSON(); 
+
+        expect(tree).toMatchSnapshot(); //asserting that the component matches the latest snapshot
+    })
+    
+})
